c41b52f13f773aa6d6b0c548fe3a259f2a59c9bf
danielg7 <danielg7@gmail.com>
2015-04-05 10:21:03 -0500

    Started to remake the vignette. Added feature to copy over dummy data.

--- R/createReportFolder.R
+++ R/createReportFolder.R
@@ -11,18 +11,34 @@
 #' 
 #' @export
 
-# Need to add checks and errors.
+# TODO:
+# Make it copy dummy data in
 
 createReportFolder <- function(Folder = getwd(), ProjectName)
   {
-  outputDir <- file.path(Folder,ProjectName) 
-  dir.create(path = outputDir)
+  if(ProjectName == NULL) stop("You need to give a project name.")
   
-  file.copy(from = system.file("extdata", "ExampleFEMOReport.Rmd", package = "rFEMO"),to = outputDir)
-  file.rename(from = file.path(outputDir,"ExampleFEMOReport.Rmd"),to = file.path(outputDir,paste(ProjectName,"_","Report",".Rmd",sep = "")))
+  outputDir <- file.path(Folder,ProjectName) 
+  
+  if(file.exists(outputDir)) {stop("Your output directory already exists!")} 
+ 
+  dir.create(path = outputDir)
   
   dir.create(path = file.path(outputDir,"Data"))
   dir.create(path = file.path(outputDir,"Data/GPS"))
   dir.create(path = file.path(outputDir,"Data/Photopoints"))
   dir.create(path = file.path(outputDir,"Data/Weather"))
+  
+  file.copy(from = system.file("extdata", "ExampleFEMOReport.Rmd", package = "rFEMO"),
+            to = outputDir)
+  
+  file.rename(from = file.path(outputDir,"ExampleFEMOReport.Rmd"),
+              to = file.path(outputDir,paste(ProjectName,"_","Report",".Rmd",sep = "")))
+  
+  file.copy(from = system.file("extdata", "YYMMDD_FireName_FirelineWeatherObservations", package = "rFEMO"),
+            to = file.path(outputDir,"Data/Weather"))
+  
+  file.rename(from = file.path(outputDir,"YYMMDD_FireName_FirelineWeatherObservations"),
+              to = file.path(outputDir,paste(ProjectName,"_","Report",".Rmd",sep = "")))
+  
   }
